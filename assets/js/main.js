ea824f4e84edfa8d18224aa041aa0a608ad2c36e
thealzhe <88514584+thealzhe@users.noreply.github.com>
2023-05-30 19:55:16 +0200

    SEO update

--- assets/js/main.js
+++ assets/js/main.js
@@ -45,3 +45,13 @@ $(".main-slider").slick({
         return $(window).width() > 990 && (o.css("height", i.height()), s.css("height", h.height())), !1
     })
 });
+/* Tawk.io */
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/64761c8ead80445890f001be/1h1mlv3e7';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
